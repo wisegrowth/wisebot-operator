990ea46f14061b9b5e0317dde0bf07e2d3113f9c
Sebastian Vera <u.verainf@gmail.com>
2017-02-03 15:53:27 -0300

    Update main.go

--- main.go
+++ main.go
@@ -17,22 +17,6 @@ var (
 	cmds command.Commands
 )
 
-func newFile(name string) (*os.File, error) {
-	file, err := os.OpenFile(name, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		if os.IsNotExist(err) {
-			file, err = os.Create(name)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			return nil, err
-		}
-	}
-
-	return file, err
-}
-
 func main() {
 	logFile, err := newFile("wisebot.logs")
 	if err != nil {
