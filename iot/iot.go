81b10dd3ccfc6b51b3c7fdac57cfa292f1fd2dfd
Sebastian Vera <u.verainf@gmail.com>
2017-02-06 12:12:34 -0300

    Refactor ios package

--- iot/iot.go
+++ iot/iot.go
@@ -12,7 +12,9 @@ import (
 )
 
 const (
+	websocketPath   = "/mqtt"
 	dateFormat      = "20060102T150405Z"
+	dateStringTempl = "%04d%02d%02d"
 	queryParamTempl = `X-Amz-Algorithm=AWS4-HMAC-SHA256
 	&X-Amz-Credential={{ .AccessKeyID }}%2F{{ .DateTime }}%2F{{ .Region }}%2F{{ .Service }}%2Faws4_request
 	&X-Amz-Date={{ .Date }}
@@ -22,7 +24,7 @@ const (
 
 var (
 	queryParamTemplate = template.Must(template.New("queryParam").Parse(queryParamTempl))
-	spaceReplacer      = strings.NewReplacer(" ", "", "\n", "", "\t", "")
+	spaceRemover       = strings.NewReplacer(" ", "", "\n", "", "\t", "")
 )
 
 // Client holds the needed data in order to connect
@@ -41,6 +43,42 @@ func (c *Client) hostname() string {
 	return fmt.Sprintf("%s:%d", strings.ToLower(c.Host), c.Port)
 }
 
+// PrepareWebSocketURL returns a signed url for aws iot service.
+// This method uses aws signature v4 to sign the url.
+func (c *Client) PrepareWebSocketURL(options *Options) string {
+	now := time.Now().UTC()
+
+	ri := &queryParamTemplateInfo{
+		Client:   c,
+		Date:     dateString(now),
+		DateTime: dateTimeString(now),
+	}
+
+	var buf []byte
+	bufw := bytes.NewBuffer(buf)
+	queryParamTemplate.Execute(bufw, ri)
+
+	queryParams := spaceRemover.Replace(string(bufw.Bytes()))
+
+	hasher := sha256.New()
+	hasher.Write([]byte(""))
+	payload := hex.EncodeToString(hasher.Sum(nil))
+
+	hostname := c.hostname()
+	canonicalHeaders := fmt.Sprintf("host:%s\n", hostname)
+	canonicalRequest := fmt.Sprintf("GET\n%s\n%s\n%s\nhost\n%s", websocketPath, queryParams, canonicalHeaders, payload)
+
+	canonicalRequestHasher := sha256.New()
+	canonicalRequestHasher.Write([]byte(canonicalRequest))
+
+	stringToSign := fmt.Sprintf("AWS4-HMAC-SHA256\n%s\n%s/%s/%s/aws4_request\n%s", dateTimeString(now), dateString(now), c.Region, c.Service, canonicalRequestHasher.Sum(nil))
+	signingKey := signatureKey(c.SecretKey, c.Region, c.Service, now)
+
+	signature := hmacSHA256(signingKey, stringToSign)
+
+	return fmt.Sprintf("%s://%s%s?%s&X-Amz-Signature=%s", options.Protocol, hostname, websocketPath, queryParams, signature)
+}
+
 // NewClient returns an initialized Client
 func NewClient(accessID, region, service, host string, port uint, t time.Time) *Client {
 	return &Client{
@@ -52,42 +90,6 @@ func NewClient(accessID, region, service, host string, port uint, t time.Time) *
 	}
 }
 
-// PrepareWebSocketURL ...
-func (c *Client) PrepareWebSocketURL(options *Options) string {
-	now := time.Now().UTC()
-
-	ri := &requestInfo{
-		Client:   c,
-		Date:     dateString(now),
-		DateTime: dateTimeString(now),
-	}
-
-	var buf []byte
-	bufw := bytes.NewBuffer(buf)
-	queryParamTemplate.Execute(bufw, ri)
-
-	queryParams := spaceReplacer.Replace(string(bufw.Bytes()))
-
-	path := "/mqtt"
-	hasher := sha256.New()
-	hasher.Write([]byte(""))
-	payload := hex.EncodeToString(hasher.Sum(nil))
-
-	hostname := c.hostname()
-	canonicalHeaders := fmt.Sprintf("host:%s\n", hostname)
-	canonicalRequest := fmt.Sprintf("GET\n%s\n%s\n%s\nhost\n%s", path, queryParams, canonicalHeaders, payload)
-
-	canonicalRequestHasher := sha256.New()
-	canonicalRequestHasher.Write([]byte(canonicalRequest))
-
-	stringToSign := fmt.Sprintf("AWS4-HMAC-SHA256\n%s\n%s/%s/%s/aws4_request\n%s", dateTimeString(now), dateString(now), c.Region, c.Service, canonicalRequestHasher.Sum(nil))
-	signingKey := signatureKey(c.SecretKey, c.Region, c.Service, now)
-
-	signature := hmacSHA256(signingKey, stringToSign)
-
-	return fmt.Sprintf("%s://%s%s?%s&X-Amz-Signature=%s", options.Protocol, hostname, path, queryParams, signature)
-}
-
 func signatureKey(secret, region, service string, date time.Time) []byte {
 	h := hmacSHA256([]byte("AWS4"+secret), dateString(date))
 	h = hmacSHA256(h, region)
@@ -95,7 +97,7 @@ func signatureKey(secret, region, service string, date time.Time) []byte {
 	return hmacSHA256(h, "aws4_request")
 }
 
-type requestInfo struct {
+type queryParamTemplateInfo struct {
 	*Client
 
 	Date     string
@@ -124,5 +126,5 @@ func dateTimeString(t time.Time) string {
 
 func dateString(t time.Time) string {
 	date := t.UTC()
-	return fmt.Sprintf("%04d%02d%02d", date.Year(), date.Month(), date.Day())
+	return fmt.Sprintf(dateStringTempl, date.Year(), date.Month(), date.Day())
 }
