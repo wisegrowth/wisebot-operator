707067c5e56e4b4bb9ee8fe15efeeae038b1a34e
Sebastian Vera <u.verainf@gmail.com>
2017-02-08 20:21:21 -0300

    Update main

--- main.go
+++ main.go
@@ -1,13 +1,92 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"os"
+	"os/signal"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/WiseGrowth/operator/command"
+	"github.com/WiseGrowth/operator/git"
+	"github.com/WiseGrowth/operator/iot"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+var (
+	commands  command.Commands
+	wisebotID string
+)
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+	wisebotID = "wisebot-id" // TODO: get id from config
+
+	commands = make(command.Commands)
+	commands.Add(
+		command.NewCommand(nil, "test-cmd", "echo", "hello world"),
+	)
+}
+
+func healthz(client MQTT.Client, message MQTT.Message) {
+	topic := message.Topic()
+
+	logger := log.WithField("topic", topic)
+	logger.Info("Received message", message.Payload())
+
+	bytes, _ := json.Marshal(&struct {
+		Data command.Commands `json:"data"`
+	}{commands})
+
+	token := client.Publish(topic+":response", byte(1), false, bytes)
+	if token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+}
 
 func main() {
-	log.Println("Hello world")
+	r := git.NewRepo(
+		"wisebots-api",
+		"git@github.com:wisegrowth/wisebots-api.git",
+	)
+
+	r.AddPostReceiveHooks(
+		r.NpmInstall(),
+		r.NpmPrune(),
+	)
+
+	client, err := iot.NewClient(
+		iot.SetHost("a55lp0huv9vtb.iot.us-west-2.amazonaws.com"),
+		iot.SetCert("tls/chechitodelboom.cert.pem"),
+		iot.SetKey("tls/chechitodelboom.private.key"),
+	)
+	check(err)
+	check(client.Connect())
+
+	check(client.Subscribe("/operator/"+wisebotID+"/healthz", healthz))
+
+	check(commands.Start())
+
+	check(r.Bootstrap())
+
+	quit := make(chan struct{})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		client.Disconnect(250)
+		log.Info("[MQTT] Disconnected")
+
+		commands.Stop()
+
+		quit <- struct{}{}
+	}()
+	<-quit
+	log.Debug("Done")
 }
 
 func check(err error) {
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		os.Exit(1)
 	}
 }
