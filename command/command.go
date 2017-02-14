9bd7a2db92d851fb1770aca84fe729e15476eca6
Sebastian Vera <u.verainf@gmail.com>
2017-02-14 13:36:12 -0300

    Remove logger, slug and add Clone function to `command`

--- command/command.go
+++ command/command.go
@@ -7,9 +7,8 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
-
-	log "github.com/Sirupsen/logrus"
 )
 
 // Status represents the current command status
@@ -25,27 +24,33 @@ const (
 	StatusStopped  Status = "stopped"
 )
 
-// Command represents a os level command, which can also
-// receive a logger file in order to dump the output to it.
+// Command represents a os level command, which can also receive a logger file
+// in order to dump the output to it.
 type Command struct {
 	Log io.WriteCloser
 	Cmd *exec.Cmd
 
-	// stderr *bytes.Buffer // Maybe this will help us debugging when a command fails?
-
-	Slug    string
 	Version string
-
-	status Status
-
 	Updater Updater
 
-	ExecName string
-	ExecArgs []string
+	// Maybe this will help us debugging when a command fails?
+	// stderr *bytes.Buffer
+
+	status   Status
+	execName string
+	execArgs []string
 }
 
-// Updater knows how to update the codebase of a specific
-// command codebase.
+// Clone clones the command by instantiate a new one with same attributes
+// and returns it. This is handy if you need to restart the process, first
+// you stop it, then clone it, then you start the new cloned process.
+func (c *Command) Clone() *Command {
+	cmd := NewCommand(c.Log, c.Version, c.execName, c.execArgs...)
+	cmd.Updater = c.Updater
+	return cmd
+}
+
+// Updater knows how to update the codebase of a specific command codebase.
 type Updater interface {
 	Update() (newVersion string, err error)
 }
@@ -57,10 +62,16 @@ func (c *Command) MarshalJSON() ([]byte, error) {
 		Version string `json:"version"`
 		Status  Status `json:"status"`
 	}{
-		Slug:    c.Slug,
+		Slug:    c.Slug(),
 		Status:  c.Status(),
 		Version: c.Version,
 	})
+}
+
+// Slug combines the command execName and execArgs in order to return a verbose
+// identifier.
+func (c *Command) Slug() string {
+	return fmt.Sprintf("%s %s", c.execName, strings.Join(c.execArgs, " "))
 }
 
 // SetStatus sets the command current status
@@ -68,16 +79,14 @@ func (c *Command) SetStatus(status Status) {
 	c.status = status
 }
 
-// Update uses the updater in order to update the code base
-// and the command version.
-// If no updater is found, it returns an error.
-// Update function returns a boolean that indicates
-// if the code was either updated or not. Knowing if
-// the command was updated is important in order to
-// decide if we need to restart it or not.
+// Update uses the updater in order to update the code base and the command
+// version.  If no updater is found, it returns an error. Update function
+// returns a boolean that indicate if the code was either updated or not.
+// Knowing if the command was updated is important in order to decide if we
+// need to restart it or not.
 func (c *Command) Update() (updated bool, err error) {
 	if c.Updater == nil {
-		return false, fmt.Errorf("command: no updater for %q command", c.Slug)
+		return false, fmt.Errorf("command: no updater for %q command", c.Slug())
 	}
 
 	oldVersion := c.Version
@@ -91,13 +100,10 @@ func (c *Command) Update() (updated bool, err error) {
 		updated = true
 	}
 
-	c.logger().Debugf("updated: %v - oldVersion: %q - newVersion: %q", updated, oldVersion, newVersion)
-
 	return updated, nil
 }
 
-// Status check the command's process state and returns
-// a verbose status.
+// Status check the command's process state and returns a verbose status.
 func (c *Command) Status() Status {
 	if c.Cmd.ProcessState == nil {
 		return c.status
@@ -116,9 +122,8 @@ func (c *Command) Status() Status {
 	return c.status
 }
 
-// CloseLog safely close the command's logger.
-// If the logger is just os.Stdout, it does not
-// close it.
+// CloseLog safely close the command's logger. If the logger is just os.Stdout,
+// it does not close it.
 func (c *Command) CloseLog() error {
 	if c.Log == nil || c.Log == os.Stdout {
 		return nil
@@ -127,26 +132,24 @@ func (c *Command) CloseLog() error {
 	return c.Log.Close()
 }
 
-// Stop stops the command and closes the log file
-// if exists.
+// Stop stops the command and closes the log file if exists.
 func (c *Command) Stop() error {
 	if c.Log != nil {
 		defer c.CloseLog()
 	}
 
 	if c.status == StatusStopped {
-		return fmt.Errorf("commands: command %q is already stopped", c.Slug)
+		return fmt.Errorf("commands: command %q is already stopped", c.Slug())
 	}
 
 	c.status = StatusStopped
 
-	c.logger().Info("Killing process")
 	if c.Cmd.Process == nil {
 		return nil
 	}
 
-	// the ProcessState only exists if either the process exited,
-	// or we called Run or Wait functions.
+	// the ProcessState only exists if either the process exited, or we called
+	// Run or Wait functions.
 	ps := c.Cmd.ProcessState
 	if ps != nil && ps.Exited() {
 		return nil
@@ -160,15 +163,13 @@ func (c *Command) Stop() error {
 	return err
 }
 
-// Wait only proxies the function call to the
-// os.Command.Wait function.
+// Wait only proxies the function call to the  os.Command.Wait function.
 func (c *Command) Wait() error {
 	return c.Cmd.Wait()
 }
 
-// Start starts the process and pipes the command's
-// output to the log file. If at any point there is an error
-// it also closes the file if exists.
+// Start starts the process and pipes the command's output to the log file.
+// If at any point there is an error it also closes the file if exists.
 func (c *Command) Start() error {
 	if c.Log == os.Stdout || c.Log == nil {
 		c.Cmd.Stdout = c.Log
@@ -196,7 +197,6 @@ func (c *Command) Start() error {
 		}()
 	}
 
-	c.logger().Info("Starting")
 	if err := c.Cmd.Start(); err != nil {
 		c.CloseLog()
 		return err
@@ -207,30 +207,21 @@ func (c *Command) Start() error {
 	return nil
 }
 
-func (c *Command) logger() *log.Entry {
-	return log.WithFields(log.Fields{
-		"command": c.Slug,
-		"version": c.Version,
-	})
-}
-
-// Success just proxies the function call to the
-// command.ProcessState struct.
+// Success just proxies the function call to the command.ProcessState struct.
 func (c *Command) Success() bool {
 	return c.Cmd.ProcessState.Success()
 }
 
 // NewCommand returns an initalized command pointer.
-func NewCommand(log io.WriteCloser, slug, version, name string, args ...string) *Command {
+func NewCommand(log io.WriteCloser, version, name string, args ...string) *Command {
 	cmd := &Command{
 		Log:     log,
 		Cmd:     exec.Command(name, args...),
-		Slug:    slug,
 		Version: version,
 
 		status:   StatusIdle,
-		ExecName: name,
-		ExecArgs: args,
+		execName: name,
+		execArgs: args,
 	}
 
 	cmd.Cmd.SysProcAttr = &syscall.SysProcAttr{
@@ -248,7 +239,5 @@ type noopUpdater struct {
 }
 
 func (nu *noopUpdater) Update() (string, error) {
-	nu.logger().Warn("Noop Updater Called")
-
 	return nu.Version, nil
 }
