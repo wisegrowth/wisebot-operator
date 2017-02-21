539bbd28bbfeae303cbfd67c0e544e033e82484f
Sebastian Vera <u.verainf@gmail.com>
2017-02-20 21:36:16 -0300

    Add routes to activate and deactivate ap mode

--- http.go
+++ http.go
@@ -24,16 +24,17 @@ type healthzMetaResponse struct {
 }
 
 type wifiStatus struct {
-	IsConnected bool   `json:"is_connected"`
-	Mode        string `json:"mode"`
+	IsConnected    bool `json:"is_connected"`
+	IsAPModeActive bool `json:"is_ap_mode_active"`
 }
 
 func newHealthResponse() *healthResponse {
 	isConnected, _ := rasp.IsConnected()
+	isAPModeActive, _ := rasp.IsAPModeActive()
 
 	meta := new(healthzMetaResponse)
 	meta.WifiStatus.IsConnected = isConnected
-	meta.WifiStatus.Mode = rasp.CurrentMode().String()
+	meta.WifiStatus.IsAPModeActive = isAPModeActive
 
 	return &healthResponse{
 		Data: services,
@@ -70,11 +71,23 @@ func getNetworksHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprouter
 	}
 }
 
-func setAPModeHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log := logger.GetLogger().WithField("handler", "setAPModeHTTPHandler")
+func activateAPModeHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log := logger.GetLogger().WithField("handler", "activateAPModeHTTPHandler")
 	log.Info("Request received")
 
-	if err := rasp.SetAPMode(); err != nil {
+	if err := rasp.ActivateAPMode(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func deactivateAPModeHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log := logger.GetLogger().WithField("handler", "deactivateAPModeHTTPHandler")
+	log.Info("Request received")
+
+	if err := rasp.DeactivateAPMode(); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -100,6 +113,7 @@ func updateNetworkHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprout
 	err := rasp.SetupWifi(network)
 	if err == nil {
 		log.Debug("Wifi Connected")
+		rasp.DeactivateAPMode()
 		// TODO: bootstrap services if it was in ap mode!
 		w.WriteHeader(http.StatusOK)
 		return
@@ -107,7 +121,7 @@ func updateNetworkHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprout
 
 	if err == rasp.ErrNoWifi {
 		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
-		if err = rasp.SetAPMode(); err != nil {
+		if err = rasp.ActivateAPMode(); err != nil {
 			log.Error("Error when setting AP Mode: " + err.Error())
 		}
 		return
@@ -122,7 +136,8 @@ func updateNetworkHTTPHandler(w http.ResponseWriter, r *http.Request, _ httprout
 // GET /healthz
 //
 // GET    /networks
-// POST   /network/ap-mode
+// POST   /network/ap-mode/activate
+// POST   /network/ap-mode/deactivate
 // PATCH  /network
 //
 func NewHTTPServer() *http.Server {
@@ -132,7 +147,9 @@ func NewHTTPServer() *http.Server {
 
 	router.GET("/networks", getNetworksHTTPHandler)
 
-	router.POST("/network/ap-mode", setAPModeHTTPHandler)
+	router.POST("/network/ap-mode/activate", activateAPModeHTTPHandler)
+	router.POST("/network/ap-mode/deactivate", deactivateAPModeHTTPHandler)
+
 	router.PATCH("/network", updateNetworkHTTPHandler)
 
 	addr := fmt.Sprintf(":%d", httpPort)
