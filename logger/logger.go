3d3d4c89feaed3e43146e18bf57f11941c831c3a
Sebastian Vera <u.verainf@gmail.com>
2017-02-27 23:35:43 -0300

    Add elasticsearch logrus hook and fix wisebot-id persistance

--- logger/logger.go
+++ logger/logger.go
@@ -3,10 +3,17 @@ package logger
 import (
 	"github.com/Sirupsen/logrus"
 	ravenSentry "github.com/evalphobia/logrus_sentry"
+	elastic "gopkg.in/olivere/elastic.v5"
+	elogrus "gopkg.in/sohlich/elogrus.v2"
 )
 
 var (
 	environment string
+)
+
+const (
+	elasticsearchURL   = "https://search-wisegrowth-jl3kj4kwv225mzcssitu47xlqe.us-west-2.es.amazonaws.com"
+	elasticsearchIndex = "wisebot-operator"
 )
 
 // Logger is the exposed standard-ish logging interface
@@ -26,7 +33,7 @@ var (
 )
 
 // setLogger sets the package level logger
-func setLogger(l *logrus.Logger) {
+func setLogger(l *logrus.Entry) {
 	log = l
 }
 
@@ -42,6 +49,20 @@ func Init(wisebotID, sentryDSN string) error {
 	log.Level = logrus.DebugLevel
 	if environment == "production" {
 		log.Formatter = &logrus.JSONFormatter{}
+		client, err := elastic.NewClient(
+			elastic.SetURL(elasticsearchURL),
+			elastic.SetScheme("https"),
+			elastic.SetSniff(false),
+		)
+		if err != nil {
+			log.Panic(err)
+		}
+		ehook, err := elogrus.NewElasticHook(client, "localhost", logrus.DebugLevel, elasticsearchIndex)
+		if err != nil {
+			return err
+		}
+
+		log.Hooks.Add(ehook)
 	}
 
 	levels := []logrus.Level{
@@ -50,16 +71,16 @@ func Init(wisebotID, sentryDSN string) error {
 		logrus.ErrorLevel,
 	}
 
-	hook, err := ravenSentry.NewAsyncWithTagsSentryHook(sentryDSN, map[string]string{"wisebot-id": wisebotID}, levels)
+	shook, err := ravenSentry.NewAsyncWithTagsSentryHook(sentryDSN, map[string]string{"wisebot-id": wisebotID}, levels)
 	if err != nil {
 		return err
 	}
 
-	hook.StacktraceConfiguration.Enable = true
+	shook.StacktraceConfiguration.Enable = true
 
-	log.Hooks.Add(hook)
-	log = log.WithField("wisebot-id", wisebotID).Logger
-	setLogger(log)
+	log.Hooks.Add(shook)
+
+	setLogger(log.WithField("wisebot-id", wisebotID))
 
 	return nil
 }
