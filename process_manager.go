bba8e6bddb550ad1a62d58e6b58a395a5c53e60f
Camilo Acuña <camilo.aku@gmail.com>
2018-01-23 16:03:53 -0300

    fix typo on KickOffServices add /update and /service-update mqtt listeners

--- process_manager.go
+++ process_manager.go
@@ -10,28 +10,28 @@ import (
 // ProcessManager is in charge of starting and stoping the processes.
 type ProcessManager struct {
 	sync.Mutex
-	Services                  *ServiceStore
-	MQTTClient                *iot.Client
-	servicesAndDaemonsStarted bool
-	mqttConnectionStarted     bool
+	Services              *ServiceStore
+	MQTTClient            *iot.Client
+	servicesStarted       bool
+	mqttConnectionStarted bool
 }
 
-// KickOffServicesAndDaemons is a function that kicks off core subprocesses
+// KickOffServices is a function that kicks off core subprocesses
 // managed by the operator. `hasInternetConnection` param indicates wether the
 // subprocesses and daemons source code can be updated by git or not.
 // The subprocesses knows how to reconnect when they lose internet connection,
 // so is not necessary to start them while being connected to the internet.
 // The wrong value of `hasInternetConnection` can raise errors, so is mandatory
 // to check if the device is online before executing this method.
-func (pm *ProcessManager) KickOffServicesAndDaemons(hasInternetConnection bool) error {
+func (pm *ProcessManager) KickOffServices(hasInternetConnection bool) error {
 	pm.Lock()
 	defer pm.Unlock()
 
 	log := logger.GetLogger()
 	log.Debug("Bootstraping and starting services")
 
-	if pm.servicesAndDaemonsStarted {
-		log.Debug("Process already started, ignoring KickOffServicesAndDaemons()")
+	if pm.servicesStarted {
+		log.Debug("Process already started, ignoring KickOffServices()")
 		return nil
 	}
 
@@ -39,7 +39,7 @@ func (pm *ProcessManager) KickOffServicesAndDaemons(hasInternetConnection bool) 
 		return err
 	}
 
-	pm.servicesAndDaemonsStarted = true
+	pm.servicesStarted = true
 
 	log.Debug("Bootstraping done")
 	return nil
@@ -120,6 +120,9 @@ func (pm *ProcessManager) bootstrapMQTTClient() error {
 	if err := pm.MQTTClient.Subscribe("/operator/"+wisebotConfig.WisebotID+"/service-update", updateServiceMQTTHandler); err != nil {
 		return err
 	}
+	if err := pm.MQTTClient.Subscribe("/operator/"+wisebotConfig.WisebotID+"/service-restart", restartServiceMQTTHandler); err != nil {
+		return err
+	}
 	if err := pm.MQTTClient.Subscribe("/operator/"+wisebotConfig.WisebotID+"/daemon-start", startDaemonMQTTHandler); err != nil {
 		return err
 	}
@@ -130,6 +133,9 @@ func (pm *ProcessManager) bootstrapMQTTClient() error {
 		return err
 	}
 	if err := pm.MQTTClient.Subscribe("/operator/"+wisebotConfig.WisebotID+"/daemon-restart", restartDaemonMQTTHandler); err != nil {
+		return err
+	}
+	if err := pm.MQTTClient.Subscribe("/operator/"+wisebotConfig.WisebotID+"/update", updateOperatorMQTTHandler); err != nil {
 		return err
 	}
 
